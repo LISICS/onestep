54728b30cbc9b666ae629201b472d8fab5bb35a4
Alexander <140154437+LISICS@users.noreply.github.com>
2023-08-31 19:09:56 +0300

    Add files via upload

---
+++ lesson №1/lesson_1_task_4.py
@@ -0,0 +1,13 @@
+#first_name = "Александр "
+#last_name = "СергеевиЧ "
+#my_n = "Меня зовут:"
+#input(my_n + first_name  +  last_name)
+
+
+ 
+
+first_name = input ('ваше имя:')
+last_name = input ('ваше отчество')
+print('Вы ввели:', first_name)
+print('Вы ввели:', last_name)
+print('Вас зовут:',first_name, last_name)
