746339aaa9bb5a6eb811f96906bb9cfb9988fa99
Alexander <140154437+LISICS@users.noreply.github.com>
2023-08-31 19:53:16 +0300

    Update lesson_1_task_4.py

--- lesson №1/lesson_1_task_4.py
+++ lesson №1/lesson_1_task_4.py
@@ -1,11 +1,3 @@
-#first_name = "Александр "
-#last_name = "СергеевиЧ "
-#my_n = "Меня зовут:"
-#input(my_n + first_name  +  last_name)
-
-
- 
-
 first_name = input ('ваше имя:')
 last_name = input ('ваше отчество')
 print('Вы ввели:', first_name)
